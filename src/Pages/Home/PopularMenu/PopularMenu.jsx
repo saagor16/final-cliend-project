c0441d91998921424484cbfa14880206933987ba
saagor16 <saagor.16@gmail.com>
2024-05-16 00:09:10 +0600

    final project first module done

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -7,7 +7,7 @@ const PopularMenu = () => {
 
   useEffect(() => {
     fetch('menu.json')
-      .then((res) => res.json)
+      .then((res) => res.json())
       .then(data => {
         const popularItems = data.filter(item => item.category === 'popular');
         setMenu(popularItems);
@@ -19,11 +19,16 @@ const PopularMenu = () => {
       <SectionTitle
         heading={"FROM OUR MENU"}
         subHeading={"Check it out"}
-      ></SectionTitle>
+      />
       <div className="gap-10 grid md:grid-cols-2">
         {menu.map((item) => (
-          <MenuItem key={item._id} item={item}></MenuItem>
+          <MenuItem key={item._id} item={item} />
         ))}
+      </div>
+      <div className="flex justify-center items-center mt-4">
+        <button className="btn btn-outline border-0 border-b-4">
+          View Full Menu
+        </button>
       </div>
     </section>
   );
